719774e56ef6eed76fdfe682e1ed7d8b880923e2
Parker <parker@kustomer.com>
2020-09-24 00:12:40 -0400

    updates

--- src/index.ts
+++ src/index.ts
@@ -12,12 +12,14 @@ export * from './middlewares/validate-request';
 
 export * from './events/base-listener';
 export * from './events/base-publisher';
-export * from './events/subjects';
-export * from './events/ticket-created-event';
-export * from './events/ticket-updated-event';
+export * from './events/TicketingApp/subjects';
+export * from './events/TicketingApp/ticket-created-event';
+export * from './events/TicketingApp/ticket-updated-event';
 
-export * from './events/types/order-status';
-export * from './events/order-created-event';
-export * from './events/order-cancelled-event';
+export * from './events/TicketingApp/types/order-status';
+export * from './events/TicketingApp/order-created-event';
+export * from './events/TicketingApp/order-cancelled-event';
 
-export * from './events/expiration-complete-event';
+export * from './events/TicketingApp/expiration-complete-event';
+
+export * from './events/TicketingApp/payment-created-event';

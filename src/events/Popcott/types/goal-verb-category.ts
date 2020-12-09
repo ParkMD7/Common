8655364873c94814f56ac1a9016d7dc5ffc018e4
Parker <parker@kustomer.com>
2020-12-08 23:44:36 -0500

    updates

---
+++ src/events/Popcott/types/goal-verb-category.ts
@@ -0,0 +1,7 @@
+export enum GoalVerbCategory {
+    Boycott = 'boycott',
+    FinanciallySupport = 'financially support',
+    EmotionallySupport = 'emotionally support',
+    LiveProtest = 'live protest',
+    None = 'none',
+};

8d00032ff2b7f3320bff8bfcf0f58c77c3b071cc
Parker <parker@kustomer.com>
2020-12-31 12:57:11 -0500

    updates

--- src/events/Popcott/goal.ts
+++ src/events/Popcott/goal.ts
@@ -1,14 +1,8 @@
 import { GoalObjective } from './types/goal-objective';
-import { RewardName } from './types/reward-name';
-import { RewardBadge } from './types/reward-badge';
-import { RewardAmount } from './types/reward-amount';
+import { Reward } from './reward';
 
 export interface Goal {
     objective: GoalObjective; // eventually separate into subject and noun
     description: string;
-    reward: {
-        name: RewardName,
-        badge: RewardBadge,
-        amount: RewardAmount,
-    }
+    reward: Reward;
 };
